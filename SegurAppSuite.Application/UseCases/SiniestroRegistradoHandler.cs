9836877f9e156eab23239d934ba2800fc6d25b0f
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-06-02 12:07:03 -0300

    Creo manejador de Eventos application

---
+++ SegurAppSuite.Application/UseCases/SiniestroRegistradoHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SegurAppSuite.Domain.Events;
+
+namespace SegurAppSuite.Application.UseCases;
+
+public class SiniestroRegistradoHandler:INotificationHandler<SiniestroRegistrado>
+{
+    public Task Handle(SiniestroRegistrado notification, CancellationToken cancellation)
+    {
+        Console.WriteLine($"Evento recibido: Siniestro {notification.SiniestroId} en Póliza {notification.PolizaId}");
+        // Aquí se puede publicar en RabbitMQ, enviar email, etc.
+        return Task.CompletedTask;
+    }
+}
